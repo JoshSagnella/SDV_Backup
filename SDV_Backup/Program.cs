427f1c2e39b9f8e03f37af628affe4617e83a4cc
Josh Sagnella <joshms@gmail.com>
2016-03-18 11:26:31 -0700

    Initial Commit

---
+++ SDV_Backup/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Compression;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //DateTime timeNow = DateTime.Now;
+
+
+            //Set up paths
+            string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string startPath = appDataPath + "\\StardewValley";
+            string zipPath = appDataPath + "\\stardew_backup\\";
+
+            //string extractPath = @"c:\example\extract";
+
+
+            
+            ZipFile.CreateFromDirectory(startPath, zipPath + DateTime.Now.ToString("MM-dd-yyyy_HH-mm-ss") + ".zip");
+            //ZipFile.CreateFromDirectory();
+            //ZipFile.ExtractToDirectory(zipPath, extractPath);
+            Console.WriteLine("All Done!");
+            //Console.ReadLine();
+            
+
+
+
+
+
+
+        }
+    }
+}
